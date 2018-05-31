b59122f4fa9297cafcd1ce2084445878ceb2c8fa
Janice Darikho <janicedarikho@gmail.com>
2018-05-31 15:05:15 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,17 @@
-
+class CashRegister
+  attr_accessor :total, :discount, :title, :price, :quantity
+  
+  def initialize(discount = 20)
+    @total = 0
+    @discount = discount
+  end
+  
+  def add_item(title, price, quantity = 1)
+    @total = @total + (price*quantity)
+  end
+  
+  def apply_discount
+    paid_percentage = ((100 - @discount).to_f)/100
+    @total * paid_percentage
+  end
+end
